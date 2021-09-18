39132e02f8bc2539915509ea716c9e7f09a3f249
Reyhaneh Sharifzadeh <sharifzadeh.reyhaneh@gmail.com>
2021-09-18 18:01:49 +0430

    Customize UserAdmin
    Fixed not hashing password

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 from user.models import User
 
-admin.site.register(User)
+
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('password',
+                           'last_login',
+                           'is_superuser',
+                           'groups',
+                           'user_permissions',
+                           'username',
+                           'email',
+                           'is_staff',
+                           'is_active',
+                           'date_joined',
+                           'is_clinic',
+                           'is_patient')}),
+    )
+
+
+admin.site.register(User, UserAdmin)
